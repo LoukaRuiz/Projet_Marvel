b41696da5c2e33098ec8d68f17c7a430cfc572b3
Louka Ruiz <louka.ruiz@ynov.com>
2021-10-27 19:14:32 +0200

    premier

---
+++ src/views/H.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+ <div v-for="item in heroes" :key="item.message">
+    {{ item.title }}
+  </div>
+  </div>
+
+</template>
+
+<script lang="ts">
+import axios from 'axios'
+
+export default {
+data() {
+    return {
+      heroes: [],
+    };
+  },
+  async created() {
+   await this.fetchUsers()
+  },
+ methods: {
+  async  fetchUsers() {
+     this.heroes=await axios.get('http://localhost:3001/heroes')
+         },
+        
+    } 
+ 
+}
+
+</script>
